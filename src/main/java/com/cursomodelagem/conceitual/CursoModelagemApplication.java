39215bbe99d08090b328f7bbb246abf20606fb7e
Patrick Almeida <patrick.automacao1@gmail.com>
2021-08-28 19:51:45 -0300

    Projeto Iniciado

---
+++ src/main/java/com/cursomodelagem/conceitual/CursoModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.cursomodelagem.conceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursomodelagem/conceitual/CursoModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursomodelagem.conceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
